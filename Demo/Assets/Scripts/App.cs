abb97816bc83e32183a1beaad8d591766c157ade
johnche(车雄生) <johnche@tencent.com>
2018-11-23 16:22:45 +0800

    xuui.app，支持模块间隔离（运行在独立沙盒，而且commands接受的数据是本module的data），模块通过exports接口交互

---
+++ Demo/Assets/Scripts/App.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using XUUI;
+
+public class App : MonoBehaviour
+{
+    Context context = null;
+
+    void Start()
+    {
+        context = new Context(@"
+            local xuui  = require 'xuui'
+            return xuui.app('myapp', {'module1', 'module2'})
+        ");
+
+        context.Attach(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        context.Dispose();
+    }
+}
